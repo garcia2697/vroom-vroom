edfd96d3ee388ed6c467dd2b3b15def527f895d9
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-10-04 22:12:34 -0500

    Installed tailwind and other dependencies for style; Created basic components file structure.

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './components/login/Login'
+import Main from './components/main/Main'
+
+const App = () => {
+  return (
+    <>
+      <Login />
+      <Main />
+    </>
+  )
+}
+
+export default App
