03b65d70205c54c944fd43f39f2a9cdf920dd75a
Adrian Alaniz <adrianalaniz@Adrians-MBP.attlocal.net>
2022-10-12 19:02:11 -0500

    graphql

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Login from "../../src/LoginForm";
+import LoginForm from "../../src/LoginForm";
 
-ReactDOM.render(<Login />, document.getElementById("root"));
+ReactDOM.render(<LoginForm />, document.getElementById("root"));
