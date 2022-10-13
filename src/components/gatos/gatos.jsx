603eb79ffa4c143a41cf0ebb8f46bf98c03bbe5f
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-10-12 21:40:15 -0500

    Updated cat cards again...

--- src/components/gatos/gatos.jsx
+++ src/components/gatos/gatos.jsx
@@ -9,10 +9,10 @@ function Gatos() {
 
   return (
     <article className='cats-item flex grid grid-cols-3'>
-      <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-        <img className="w-fill object-cover h-96" src="" alt="" id="perro-img" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Kitty Cat</div>
+        <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
+          <img className="w-fill object-cover h-96" src="" alt="" id="gato-img" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Kitty Cat</div>
             <h3 className="text-gray-700 text-base">Tabby</h3>
             <h3 className="text-gray-700 text-base">Age: 7 months.</h3>
           </div>
@@ -24,7 +24,7 @@ function Gatos() {
         </div>
 
         <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-          <img className="w-fill object-cover h-96" src="" alt="" id="perro-img" />
+          <img className="w-fill object-cover h-96" src="" alt="" id="gato-img" />
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">Marf</div>
               <h3 className="text-gray-700 text-base">British Shorthair</h3>
@@ -33,12 +33,12 @@ function Gatos() {
             <div className="px-6 pt-4 pb-2">
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#meow</span>
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#shorthairshawty</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#chunk</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#</span>
             </div>
           </div>
 
           <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-            <img className="w-fill object-cover h-96" src="husky-img.jpeg" alt="" id="perro-img" />
+            <img className="w-fill object-cover h-96" src="husky-img.jpeg" alt="" id="gato-img" />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">Doggo</div>
                 <h3 className="text-gray-700 text-base">Husky</h3>
@@ -52,21 +52,21 @@ function Gatos() {
             </div>
 
             <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-        <img className="w-fill object-cover h-96" src="husky-img.jpeg" alt="" id="perro-img" />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Doggo</div>
-            <h3 class="text-gray-700 text-base">Husky</h3>
-            <h3 class="text-gray-700 text-base">Age: 1 year</h3>
-          </div>
-          <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
-          </div>
+              <img className="w-fill object-cover h-96" src="husky-img.jpeg" alt="" id="gato-img" />
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Doggo</div>
+                  <h3 class="text-gray-700 text-base">Husky</h3>
+                  <h3 class="text-gray-700 text-base">Age: 1 year</h3>
+                </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
+              </div>
         </div>
 
         <div class="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-          <img class="w-fill object-cover h-96" src="husky-img.jpg" alt="" id="perro-img"></img>
+          <img class="w-fill object-cover h-96" src="husky-img.jpg" alt="" id="gato-img"></img>
           <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">Doggo</div>
               <h3 class="text-gray-700 text-base">Husky</h3>
@@ -80,7 +80,7 @@ function Gatos() {
           </div>
 
           <div class="max-w-sm p-4 rounded overflow-hidden shadow-lg">
-            <img class="w-fill object-cover h-96" src="husky-img.jpeg" alt="" id="perro-img" />
+            <img class="w-fill object-cover h-96" src="husky-img.jpeg" alt="" id="gato-img" />
             <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2">Doggo</div>
                 <h3 class="text-gray-700 text-base">Husky</h3>
