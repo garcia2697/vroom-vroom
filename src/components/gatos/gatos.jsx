0064f36a7bfbdb07a879c90916ec50fd12db9f1f
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-10-12 21:34:02 -0500

    Updating cat cards.

--- src/components/gatos/gatos.jsx
+++ src/components/gatos/gatos.jsx
@@ -12,28 +12,28 @@ function Gatos() {
       <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
         <img className="w-fill object-cover h-96" src="" alt="" id="perro-img" />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Doggo aka</div>
-            <h3 className="text-gray-700 text-base">Husky</h3>
-            <h3 className="text-gray-700 text-base">Age: 1 year</h3>
+          <div className="font-bold text-xl mb-2">Kitty Cat</div>
+            <h3 className="text-gray-700 text-base">Tabby</h3>
+            <h3 className="text-gray-700 text-base">Age: 7 months.</h3>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#gato</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#kittycat</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#clawme</span>
           </div>
         </div>
 
         <div className="max-w-sm p-4 rounded overflow-hidden shadow-lg">
           <img className="w-fill object-cover h-96" src="" alt="" id="perro-img" />
           <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Doggo</div>
-              <h3 className="text-gray-700 text-base">Husky</h3>
-              <h3 className="text-gray-700 text-base">Age: 1 year</h3>
+            <div className="font-bold text-xl mb-2">Marf</div>
+              <h3 className="text-gray-700 text-base">British Shorthair</h3>
+              <h3 className="text-gray-700 text-base">Age: 2 years.</h3>
             </div>
             <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#pupper</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#husky</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bark512</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#meow</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#shorthairshawty</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#chunk</span>
             </div>
           </div>
 
